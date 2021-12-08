84cea3bca62aacb86e2c6127ade9babf54f07023
tbellenger <tbellenger@gmail.com>
2021-12-07 20:39:21 -0800

    added category to data returned in findAll products

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -8,7 +8,10 @@ router.get("/", async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
   try {
-    const product = await Product.findAll();
+    const product = await Product.findAll({
+      attributes: ["id", "product_name", "price", "stock"],
+      include: [{ model: Category, attributes: ["id", "category_name"] }],
+    });
     res.json(product);
   } catch (err) {
     res.json(err);
